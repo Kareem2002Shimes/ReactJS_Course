a70bfc815880e4c40e7dab07db8537f04e471ddd
Kareem2002Shimes <kareem4shimas@gmail.com>
2023-07-03 15:59:26 +0300

    useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,63 @@
-import { createContext } from "react";
-import ComponentA from "./components/ComponentA";
+import { useState } from "react";
+import { useReducer } from "react";
 
-export const UserContext = createContext();
-export const ChannelContext = createContext();
+const initialState = {
+  firstCount: 0,
+  secoundCount: 2,
+};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return {
+        firstCount: state.firstCount + action.payload,
+        secoundCount: state.secoundCount + action.payload,
+      };
+    case "decrement":
+      return {
+        firstCount: state.firstCount - action.payload,
+        secoundCount: state.secoundCount - action.payload,
+      };
+    case "reset":
+      return initialState;
+    default:
+      return state;
+  }
+};
+
 function App() {
+  const [countOne, dispatch] = useReducer(reducer, initialState);
+  const [countTwo, dispatchCountTwo] = useReducer(reducer, initialState);
   return (
     <div>
-      <UserContext.Provider
-        value={{ username: "Kareem", position: "Frontend Developer" }}
-      >
-        <ChannelContext.Provider value={"SAC"}>
-          <ComponentA />
-        </ChannelContext.Provider>
-      </UserContext.Provider>
+      {/* Count One */}
+      <div>
+        <h1>Count One - {countOne.firstCount}</h1>
+        <button onClick={() => dispatch({ type: "increment", payload: 1 })}>
+          Increment
+        </button>
+        <button onClick={() => dispatch({ type: "decrement", payload: 1 })}>
+          Decrement
+        </button>
+        <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
+      </div>
+      {/* Count Two */}
+
+      <div>
+        <h1>Count Two - {countTwo.secoundCount}</h1>
+        <button
+          onClick={() => dispatchCountTwo({ type: "increment", payload: 2 })}
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => dispatchCountTwo({ type: "decrement", payload: 2 })}
+        >
+          Decrement
+        </button>
+        <button onClick={() => dispatchCountTwo({ type: "reset" })}>
+          Reset
+        </button>
+      </div>
     </div>
   );
 }
